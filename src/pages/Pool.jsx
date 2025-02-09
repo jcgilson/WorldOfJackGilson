f766384b5841ed6c9d82ef9950d6bddf9e52539e
jcgilson <jcgilson@iu.edu>
2025-02-09 10:51:13 -0700

    feat: prevent displaying pool entry form when tournament is in progress

--- src/pages/Pool.jsx
+++ src/pages/Pool.jsx
@@ -756,7 +756,7 @@ const Pool = () => {
                         if (
                             (
                                 // It's been at least 15 minutes since last fetch
-                                ((timestamp - response.data.timestamp) > 1) // fix this
+                                ((timestamp - response.data.timestamp) > 900000)
                                 // It's a tournament day
                                 && isReadyToGetUpdatedLeaderboardInfo
                                 // Round is not in "Official" (completed) status
@@ -1280,8 +1280,8 @@ const Pool = () => {
                 headers: {
                     'x-rapidapi-host': 'live-golf-data.p.rapidapi.com',
                     // 'x-rapidapi-key': '0598eb6b02msh5b4a6094ffc4e05p1c4f7djsnb241d4f5f1fe' // .n
-                    'x-rapidapi-key': '61ed57e536msh30168bd1f1e8ffep126523jsnfd01ffcdc1bf' // 97
-                    // 'x-rapidapi-key': '63d42b3368msh0f38118424b82bap14014ajsn0b2ea7d4f38a' // ggp
+                    // 'x-rapidapi-key': '61ed57e536msh30168bd1f1e8ffep126523jsnfd01ffcdc1bf' // 97
+                    'x-rapidapi-key': '63d42b3368msh0f38118424b82bap14014ajsn0b2ea7d4f38a' // ggp
                 }
             };
               
